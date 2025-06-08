7949edf01967a1d15656e3c589d13d4d92739126
Ahmad Alfarel <assaniry202@gmail.com>
2025-06-08 18:02:00 +0700

    feat: implement gRPC server setup and registration for GreetService

--- server/main.go
+++ server/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/ocdsquad/golang-grpc/proto" // Adjust the import path to your actual project structure
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8084"
+)
+
+type server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &server{})
+	log.Printf("Server is listening on %s", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	// Register your gRPC services here
+
+}
