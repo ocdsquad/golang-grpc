60d2abd80943a8195a1f2a17d2f78013d4ba3476
Ahmad Alfarel <assaniry202@gmail.com>
2025-06-08 18:01:25 +0700

    feat: add unary client function to call Greet service

---
+++ client/unary.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/ocdsquad/golang-grpc/proto" // Adjust the import path to your actual project structure
+)
+
+func callSayHello(client pb.GreetServiceClient) {
+	// Create a request with no parameters
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// Set a timeout for the request
+	// ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+	// Note: The context.WithCancel function is used to create a context that can be cancelled.
+	// This is useful for setting a timeout or for cancelling the request if needed.
+	defer cancel()
+	req := &pb.NoParam{}
+
+	// Call the SayHello method
+	resp, err := client.Greet(ctx, req)
+	if err != nil {
+		log.Fatalf("Error calling Greet: %v", err)
+	}
+
+	// Print the response message
+	log.Printf("Response from Greet: %s", resp.Message)
+}
