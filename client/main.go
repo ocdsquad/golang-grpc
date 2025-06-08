64a50df552cae5e75f54c293a2860cbd82b0f1a9
Ahmad Alfarel <assaniry202@gmail.com>
2025-06-08 18:01:18 +0700

    feat: implement gRPC client connection with transport credentials

--- client/main.go
+++ client/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/ocdsquad/golang-grpc/proto" // Adjust the import path to your actual project structure
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8084"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// conn, err := grpc.Dial("localhost"+port, grpc.WithInsecure()) // Deprecated, use WithTransportCredentials instead
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+	log.Printf("Client connected to server on %s", port)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{"Alice", "Bob", "Charlie"},
+	// }
+
+	callSayHello(client)
+}
